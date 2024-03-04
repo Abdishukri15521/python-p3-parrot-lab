2ec236d0492ba330bf458fe570c7f1d5e5b62bdf
abdishukri.mohamed1 <hillow15521@gmail.com>
2024-03-04 14:31:30 +0300

    completed lab

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,28 @@
-def parrot():
-    pass
+def parrot(arg="Squawk!"):
+    print(arg)
+    return arg
+
+class TestParrot:
+    def test_prints_argument(self):
+        '''prints the argument passed to it.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        parrot("Hello!")
+        sys.stdout = sys.__stdout__
+        assert captured_out.getvalue() == "Hello!\n"
+
+    def test_returns_argument(self):
+        '''returns the argument passed to it.'''
+        assert parrot("Hello!") == "Hello!"
+
+    def test_prints_squawk_by_default(self):
+        '''prints "Squawk!" if no argument is passed.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        parrot()
+        sys.stdout = sys.__stdout__
+        assert captured_out.getvalue() == "Squawk!\n"
+
+    def test_returns_squawk_by_default(self):
+        '''returns "Squawk!" if no argument is passed.'''
+        assert parrot() == "Squawk!"
